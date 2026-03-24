a33bf3e789bca54193881c8d97fb69f4f94638df
Matt Brown <mattbrown@wewebworkers.com>
2026-03-24 15:31:01 -0400

    Update metadataBase to play.mtgguessr.io

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_BASE_URL || "https://mtgguessr.io"
+    process.env.NEXT_PUBLIC_BASE_URL || "https://play.mtgguessr.io"
   ),
   title: "MTG Guess the Card",
   description:
